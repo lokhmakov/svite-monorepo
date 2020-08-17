25830fef97fd62491276b78af0c7f9f8cabf86d5
lokhmakov <l.pavel.m@gmail.com>
2020-08-17 11:28:58 +0300

    Remove svite .git folder and add files

---
+++ apps/client/src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  import process from "@svite-monorepo/screens";
+  import { v4 as uuid } from "uuid";
+
+  const world = uuid();
+</script>
+
+<style>
+  h1 {
+    color: orangered; /* change color an save to see hmr update */
+  }
+</style>
+
+<h1>Hello {world}</h1>
+<p>Open App.svelte in your editor and change something to see HMR in action</p>
+
+<button on:click={process}>Click me</button>
