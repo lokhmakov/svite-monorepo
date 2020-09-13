7ad7ed19d7d75a704705977e3ce62d03ae9aa464
lokhmakov <l.pavel.m@gmail.com>
2020-09-13 19:42:44 +0200

    learn-d3-by-example

--- apps/client/src/screens/screen--home.svelte
+++ apps/client/src/screens/screen--home.svelte
@@ -1,14 +1,18 @@
 <script>
-  import {BlockEnter} from 'auth/ui'
-  import {BlockPmNodeListView} from 'pm--node/ui'
+  // import {BlockEnter} from 'auth/ui'
+  // import {BlockPmNodeListView} from 'pm--node/ui'
 
-  import SectionCenter from 'sections/section--center.svelte'
+  // import SectionCenter from 'sections/section--center.svelte'
+
+  import SectionD31 from 'evoke-me--d3/ui/section--1--by-example.svelte'
 </script>
 
-<SectionCenter>
+<!-- <SectionCenter>
   <BlockEnter />
 </SectionCenter>
 
 <SectionCenter>
   <BlockPmNodeListView />
-</SectionCenter>
+</SectionCenter> -->
+
+<SectionD31 />
